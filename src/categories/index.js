9eaf94a8284b1e72a4fda9394a5ac3598547bfd0
AndyCotruta <acotruta18@gmail.com>
2023-03-13 19:11:13 +0100

    added cloudinary for categories

--- src/categories/index.js
+++ src/categories/index.js
@@ -1,15 +1,27 @@
 import express from "express";
 import httpErrors from "http-errors";
-
 import CategoriesModel from "../categories/model.js";
+import multer from "multer";
+import { v2 as cloudinary } from "cloudinary";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+
+const cloudinaryCategory = multer({
+  storage: new CloudinaryStorage({
+    cloudinary, // cloudinary is going to search in .env vars for smt called process.env.CLOUDINARY_URL
+    params: {
+      folder: "bamboobites/categories",
+    },
+  }),
+}).single("categoryImage");
 
 const categoriesRouter = express.Router();
 
 const { NotFound, Unauthorized, BadRequest } = httpErrors;
 
-categoriesRouter.post("/", async (req, res, next) => {
+categoriesRouter.post("/", cloudinaryCategory, async (req, res, next) => {
   try {
-    const newCategory = new CategoriesModel(req.body);
+    const url = req.file.path;
+    const newCategory = new CategoriesModel({ ...req.body, image: url });
     const { _id } = await newCategory.save();
     res.status(201).send(`Category with id ${_id} was successfully created`);
   } catch (error) {
@@ -49,24 +61,29 @@ categoriesRouter.get("/:categoryId", async (req, res, next) => {
   }
 });
 
-categoriesRouter.put("/:categoryId", async (req, res, next) => {
-  try {
-    const categoryId = req.params.categoryId;
-    const updatedCategory = await CategoriesModel.findByIdAndUpdate(
-      categoryId,
-      req.body,
-      { new: true, runValidators: true }
-    );
-    if (updatedCategory) {
-      res.status(201).send(updatedCategory);
-    } else {
-      next(NotFound(`Category with id ${categoryId} was not found`));
+categoriesRouter.put(
+  "/:categoryId",
+  cloudinaryCategory,
+  async (req, res, next) => {
+    try {
+      const url = req.file.path;
+      const categoryId = req.params.categoryId;
+      const updatedCategory = await CategoriesModel.findByIdAndUpdate(
+        categoryId,
+        { ...req.body, image: url },
+        { new: true, runValidators: true }
+      );
+      if (updatedCategory) {
+        res.status(201).send(updatedCategory);
+      } else {
+        next(NotFound(`Category with id ${categoryId} was not found`));
+      }
+    } catch (error) {
+      console.log(error);
+      next(error);
     }
-  } catch (error) {
-    console.log(error);
-    next(error);
   }
-});
+);
 
 categoriesRouter.delete("/:categoryId", async (req, res, next) => {
   try {
--- src/categories/model.js
+++ src/categories/model.js
@@ -6,7 +6,7 @@ const categorySchema = new Schema(
   {
     name: { type: String, required: true },
     short_description: { type: String, required: true },
-    image: { type: String, required: true },
+    image: { type: String },
     restaurants: [{ type: Schema.Types.ObjectId, ref: "Restaurant" }],
   },
   { timestamps: true }
