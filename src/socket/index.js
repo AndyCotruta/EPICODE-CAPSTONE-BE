8c523741c0d37fb26e95477ea778dd16bcc76bd1
AndyCotruta <acotruta18@gmail.com>
2023-03-15 17:02:50 +0100

    small change

--- src/socket/index.js
+++ src/socket/index.js
@@ -1,7 +1,7 @@
 export const newConnectionHandler = (newClient) => {
   console.log("NEW CONNECTION: ", newClient.id);
   // 1. We emit a welcome event to the client to make sure connection is on
-  newClient.emit("welcome", {
+  newClient.emit("connected", {
     message: `Welcome to Bamboo Bites shared order!, ${newClient.id}`,
   });
 
